1de0cb82e8e03e1213a476d11aff279f23d436e0
Victor Alexandre <49332954+vyctor922@users.noreply.github.com>
2021-11-26 15:36:43 -0500

    downgrade pesado pt.2

---
+++ js/personalizado.js
@@ -0,0 +1,130 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: 'pt-br',
+        header: {
+            left: 'prev,next',
+            center: 'title',
+            right: 'today,dayGridMonth,listYear' //listYear não funciona
+        },
+        plugins: ['interaction', 'dayGrid'],
+        //editable: true,
+        eventLimit: true,
+        events: 'listar_eventos.php',
+
+        extraParams: function () {
+            return {
+                cachebuster: new Date().valueOf()
+            };
+        },
+
+        eventClick: function (info) {
+            info.jsEvent.preventDefault();
+            $("#apagar_evento").attr("href", "deletar_evento.php?id=" + info.event.id);
+            $('#visualizar #id').text(info.event.id);
+            $('#visualizar #id').val(info.event.id);
+            $('#visualizar #title').text(info.event.title);
+            $('#visualizar #title').val(info.event.title);
+            $('#visualizar #start').text(info.event.start.toLocaleString());
+            $('#visualizar #start').val(info.event.start.toLocaleString());
+            $('#visualizar #end').text(info.event.end.toLocaleString());
+            $('#visualizar #end').val(info.event.end.toLocaleString());
+            $('#visualizar #color').val(info.event.backgroundColor);
+            $('#visualizar').modal('show');
+        },
+        default: 100, //teste de delay
+
+        selectable: true,
+        select: function (info) {
+            $('#cadastrar #start').val(info.start.toLocaleString());
+            $('#cadastrar #end').val(info.end.toLocaleString());
+            $('#cadastrar').modal('show');
+        }
+    });
+
+    calendar.render();
+});
+
+function DataHora(evento, objeto) {
+    var keypress = (window.event) ? event.keyCode : evento.which;
+    campo = eval(objeto);
+    if (campo.value == '00/00/0000 00:00:00') {
+        campo.value = "";
+    }
+
+    caracteres = '0123456789';
+    separacao1 = '/';
+    separacao2 = ' ';
+    separacao3 = ':';
+    conjunto1 = 2;
+    conjunto2 = 5;
+    conjunto3 = 10;
+    conjunto4 = 13;
+    conjunto5 = 16;
+
+    if ((caracteres.search(String.fromCharCode(keypress)) != -1) && campo.value.length < (19)) {
+        if (campo.value.length == conjunto1)
+            campo.value = campo.value + separacao1;
+        else if (campo.value.length == conjunto2)
+            campo.value = campo.value + separacao1;
+        else if (campo.value.length == conjunto3)
+            campo.value = campo.value + separacao2;
+        else if (campo.value.length == conjunto4)
+            campo.value = campo.value + separacao3;
+        else if (campo.value.length == conjunto5)
+            campo.value = campo.value + separacao3;
+    } else {
+        event.returnValue = false;
+    }
+}
+
+$(document).ready(function () {
+    $("#addevent").on("submit", function (event) {
+        event.preventDefault();
+        $.ajax({
+            method: "POST",
+            url: "criar_evento.php",
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+
+            success: function (retorna) {
+                if (retorna['sit']) {
+                    location.reload();
+                } else {
+                    $("#msg-add").html(retorna['msg']);
+                }
+            }
+        })
+    });
+
+    $('.btn-canc-vis').on("click", function () {
+        $('.visevent').slideToggle();
+        $('.formedit').slideToggle();
+    })
+
+    $('.btn-canc-edit').on("click", function () {
+        $('.formedit').slideToggle();
+        $('.visevent').slideToggle();
+    })
+
+    $("#editevent").on("submit", function (event) {
+        event.preventDefault();
+        $.ajax({
+            method: "POST",
+            url: "editar_evento.php",
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+
+            success: function (retorna) {
+                if (retorna['sit']) {
+                    location.reload();
+                } else {
+                    $("#msg-edit").html(retorna['msg']);
+                }
+            }
+        })
+    });
+});
